3b83aff86f5a158a9054119667e7f8a4eecd28d0
Hassan Sh. AlShammari <100683964+Al3boodd@users.noreply.github.com>
2022-08-03 19:42:40 +0300

    70% done

---
+++ My project (2)/Assets/Scenes/cs.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cs : MonoBehaviour
+{
+
+
+    string heroName = "spiderman ";
+    int heroPower = 10;
+
+
+    string villainName = "venom ";
+    int villainPower = 20;
+    float playerSpeed = 2.5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(" the hero name is " + heroName + " and the villain name is " + villainName);
+        if (heroPower > villainPower)
+        {
+            print(" and the hero is stronger ");
+        }
+        else
+        {
+            print(" and the villain is stronger ");
+        }
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SetSpeed(float newSpeed)
+    {
+        print("old player speed is " + playerSpeed);
+        playerSpeed = newSpeed;
+        print("new player speed is " + playerSpeed);
+    }
+}
